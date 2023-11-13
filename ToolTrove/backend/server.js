a8b6fd385fbaf5826d2510882f9b41ad37d46ea8
SARATH0899 <sarathmr16@gmail.com>
2023-11-13 21:32:34 +0530

    Backend Authentication using JWT webToken

--- ToolTrove/backend/server.js
+++ ToolTrove/backend/server.js
@@ -1,9 +1,11 @@
 import  express  from "express";
 import dotenv from 'dotenv';
+import cookieParser from "cookie-parser";
 dotenv.config();
 import connectDB from "./config/db.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import productRoutes from './routes/productRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 
 
 const port = process.env.PORT || 5000;
@@ -11,11 +13,20 @@ const port = process.env.PORT || 5000;
 connectDB(); // Connect to MongoDB
 
 const app = express();
+
+//Body Parser Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Cookie parser middleware
+app.use(cookieParser());
+
 app.get('/', (req, res) => {
     res.send('API is Running...');
 });
 
 app.use('/api/products', productRoutes);
+app.use('/api/users', userRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
