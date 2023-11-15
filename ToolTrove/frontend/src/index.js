1c1d040111da4ffcb0ea289c7bd446eb4a173e25
SARATH0899 <sarathmr16@gmail.com>
2023-11-15 14:51:45 +0530

    Frontend Authentication

--- ToolTrove/frontend/src/index.js
+++ ToolTrove/frontend/src/index.js
@@ -16,8 +16,8 @@ import reportWebVitals from './reportWebVitals';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
 import CartScreen from './screens/CartScreen';
-//import LoginScreen from './screens/LoginScreen';
-//import RegisterScreen from './screens/RegisterScreen';
+import LoginScreen from './screens/LoginScreen';
+import RegisterScreen from './screens/RegisterScreen';
 
 
 const router = createBrowserRouter(
@@ -26,6 +26,8 @@ const router = createBrowserRouter(
       <Route index={true} path="/" element={<HomeScreen />} />
       <Route path="/products/:id" element={<ProductScreen />} />
       <Route path="/cart" element={<CartScreen />} />
+      <Route path="/login" element={<LoginScreen />} />
+      <Route path="/register" element={<RegisterScreen />} />
 
         
         </Route>
