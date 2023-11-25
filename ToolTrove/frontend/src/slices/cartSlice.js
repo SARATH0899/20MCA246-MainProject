7c7c1a713ab3dd4e59b7acda24f05bdab96eebb0
SARATH0899 <sarathmr16@gmail.com>
2023-11-25 12:14:03 +0530

    CheckOut Process & Payment + PlaceOrder Screen

--- ToolTrove/frontend/src/slices/cartSlice.js
+++ ToolTrove/frontend/src/slices/cartSlice.js
@@ -3,7 +3,7 @@ import { updateCart } from '../utils/cartUtils';
 
 const initialState = localStorage.getItem('cart')
   ? JSON.parse(localStorage.getItem('cart'))
-  : { cartItems: [] };//, shippingAddress: {}, paymentMethod: 'PayPal' };
+  : { cartItems: [] , shippingAddress: {}, paymentMethod: 'PayPal' };
 
 const cartSlice = createSlice({
   name: 'cart',
@@ -29,44 +29,35 @@ const cartSlice = createSlice({
       state.cartItems = state.cartItems.filter((x) => x._id !== action.payload);
       return updateCart(state);
     },
-    
-},
-});
-
-
-
-      /*return updateCart(state, item);
-    },
-    removeFromCart: (state, action) => {
-      state.cartItems = state.cartItems.filter((x) => x._id !== action.payload);
-      return updateCart(state);
-    },
     saveShippingAddress: (state, action) => {
       state.shippingAddress = action.payload;
-      localStorage.setItem('cart', JSON.stringify(state));
+      return updateCart(state);
+      //localStorage.setItem('cart', JSON.stringify(state));
     },
     savePaymentMethod: (state, action) => {
       state.paymentMethod = action.payload;
-      localStorage.setItem('cart', JSON.stringify(state));
+      return updateCart(state);
+      //localStorage.setItem('cart', JSON.stringify(state));
     },
     clearCartItems: (state, action) => {
       state.cartItems = [];
-      localStorage.setItem('cart', JSON.stringify(state));
+      return updateCart(state);
+      //localStorage.setItem('cart', JSON.stringify(state));
     },
     // NOTE: here we need to reset state for when a user logs out so the next
     // user doesn't inherit the previous users cart and shipping
-    resetCart: (state) => (state = initialState),
+    //resetCart: (state) => (state = initialState),
+  
   },
 });
 
-export const {
-  addToCart,
-  removeFromCart,
-  saveShippingAddress,
-  savePaymentMethod,
-  clearCartItems,
+
+
+   
+
+/*export const {
   resetCart,
 } = cartSlice.actions;*/
-export const { addToCart, removeFromCart } = cartSlice.actions;
+export const { addToCart, removeFromCart, saveShippingAddress, savePaymentMethod, clearCartItems } = cartSlice.actions;
 
 export default cartSlice.reducer;
