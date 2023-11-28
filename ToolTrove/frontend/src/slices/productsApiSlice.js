ae5061b38d0500f47362222d0ddb859db4c13d51
SARATH0899 <sarathmr16@gmail.com>
2023-11-28 18:12:40 +0530

    Admin _ Delete, Update, upload products

--- ToolTrove/frontend/src/slices/productsApiSlice.js
+++ ToolTrove/frontend/src/slices/productsApiSlice.js
@@ -1,4 +1,4 @@
-import { PRODUCTS_URL } from '../constants';
+import { PRODUCTS_URL, UPLOAD_URL } from '../constants';
 import { apiSlice } from "./apiSlice";
 
 export const productsApiSlice = apiSlice.injectEndpoints({
@@ -7,6 +7,7 @@ export const productsApiSlice = apiSlice.injectEndpoints({
       query: () => ({
         url: PRODUCTS_URL,
       }),
+      providesTags: ['Product'],
       keepUnusedDataFor: 5,
     }),
     getproductDetails: builder.query({
@@ -14,8 +15,44 @@ export const productsApiSlice = apiSlice.injectEndpoints({
         url: `${PRODUCTS_URL}/${productId}`,
       }),
       keepUnusedDataFor: 5,
-    })
+    }),
+    createProduct: builder.mutation({
+      query: () => ({
+        url: PRODUCTS_URL,
+        method: 'POST',
+      }),
+      invalidatesTags: ['Product'],
+    }),
+    updateProduct: builder.mutation({
+      query: (data) => ({
+        url: `${PRODUCTS_URL}/${data.productId}`,
+        method: 'PUT',
+        body: data,
+      }),
+      invalidatesTags: ['product'],
+    }),
+    uploadProductImage: builder.mutation({
+      query: (data) => ({
+        url: `${UPLOAD_URL}`,
+        method: 'POST',
+        body: data,
+      }),
+    }),
+    deleteProduct: builder.mutation({
+      query: (productId) => ({
+        url: `${PRODUCTS_URL}/${productId}`,
+        method: 'DELETE',
+      }),
+      providesTags: ['Product'],
+    }),
   }),
 });
 
-export const { useGetProductsQuery, useGetproductDetailsQuery } = productsApiSlice;
+export const { 
+  useGetProductsQuery, 
+  useGetproductDetailsQuery, 
+  useCreateProductMutation, 
+  useUpdateProductMutation, 
+  useUploadProductImageMutation,
+  useDeleteProductMutation,
+} = productsApiSlice;
