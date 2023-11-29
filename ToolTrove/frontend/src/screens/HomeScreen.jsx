adc882802d60634d5c5a15fe547400f30b848b5a
SARATH0899 <sarathmr16@gmail.com>
2023-11-29 19:34:52 +0530

    Search Component Added

--- ToolTrove/frontend/src/screens/HomeScreen.jsx
+++ ToolTrove/frontend/src/screens/HomeScreen.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Row, Col } from 'react-bootstrap';
 import { useParams } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import Product from '../components/Product';
 import Loader from '../components/Loader';
 import Message from '../components/Message';
@@ -9,12 +10,17 @@ import { useGetProductsQuery } from '../slices/productsApiSlice';
 
 
 const HomeScreen = () => {
-  const { pageNumber } = useParams();
+  const { pageNumber, keyword } = useParams();
 
-  const { data, isLoading, error } = useGetProductsQuery({ pageNumber });
+  const { data, isLoading, error } = useGetProductsQuery({ keyword, pageNumber });
 
   return (
     <>
+     {keyword && (
+        <Link to='/' className='btn btn-light mb-4'>
+          Go Back
+        </Link>
+      )}
       { isLoading ? (
         <Loader />
       ) : error ? (
@@ -34,7 +40,8 @@ const HomeScreen = () => {
         </Row>
         <Paginate
             pages={data.pages}
-            page={data.page} />
+            page={data.page} 
+            keyword ={keyword ? keyword : '' } />
       </>
       )}  
     </>
