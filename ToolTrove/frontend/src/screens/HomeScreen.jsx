1466912113f824ac39bfe49b0ab55f409e24f730
SARATH0899 <sarathmr16@gmail.com>
2023-11-29 18:33:50 +0530

    Paginate component Added for Products

--- ToolTrove/frontend/src/screens/HomeScreen.jsx
+++ ToolTrove/frontend/src/screens/HomeScreen.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import { Row, Col } from 'react-bootstrap';
+import { useParams } from 'react-router-dom';
 import Product from '../components/Product';
 import Loader from '../components/Loader';
 import Message from '../components/Message';
+import Paginate from '../components/Paginate';
 import { useGetProductsQuery } from '../slices/productsApiSlice';
 
 
 const HomeScreen = () => {
-  const { data: products, isLoading, error } = useGetProductsQuery();
+  const { pageNumber } = useParams();
+
+  const { data, isLoading, error } = useGetProductsQuery({ pageNumber });
 
   return (
     <>
@@ -21,14 +25,18 @@ const HomeScreen = () => {
         <>
         <h1>Latest Products</h1>
         <Row>
-            {products.map((product) =>(
+            {data.products.map((product) =>(
                 <Col key={product._id} sm={6} md={4} lg={3} xl={2}>
                     <Product productData={product} />
                 </Col>
 
             ))}
         </Row>
-      </>) }  
+        <Paginate
+            pages={data.pages}
+            page={data.page} />
+      </>
+      )}  
     </>
 
     
