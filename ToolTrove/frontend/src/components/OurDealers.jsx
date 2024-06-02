163d3372d10aadbe3c28bba282bd0439c978d49b
SARATH0899 <117584193+SARATH0899@users.noreply.github.com>
2024-06-02 22:41:42 +0530

    updated dealers

--- ToolTrove/frontend/src/components/OurDealers.jsx
+++ ToolTrove/frontend/src/components/OurDealers.jsx
@@ -3,43 +3,79 @@ import { Col } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { FaInstagram, FaTwitter, FaGlobe } from "react-icons/fa";
 import "../assets/styles/our_dealers.css";
-import bosch from "../assets/images/bosch.png";
+import bosch from "../assets/images/bosch.jpg";
 import dewalt from "../assets/images/dewalt.png";
 import black_decker from "../assets/images/black_decker.png";
 import hitachi from "../assets/images/hitachi.png";
+import planet_power from "../assets/images/planet_power.png";
+import makita from "../assets/images/makita.png";
+import ferm from "../assets/images/ferm.png";
+import dongcheng from "../assets/images/dongcheng.jpeg";
 
 const OUR_DEALERS = [
   {
-    name: "Bosch",
-    since: "Since 2001",
-    instaUrl: "#",
-    twitUrl: "#",
-    website: "#",
-    imgUrl: bosch,
-  },
-  {
-    name: "Hitachi",
-    since: "Since 2003",
-    instaUrl: "#",
-    twitUrl: "#",
-    website: "#",
-    imgUrl: hitachi,
-  },
-  {
     name: "Dewalt",
-    since: "Since 2005",
+    since: "Since 2001",
     instaUrl: "#",
     twitUrl: "#",
     website: "#",
     imgUrl: dewalt,
   },
   {
-    name: "Black-Decker",
+    name: "Black & Decker",
     since: "Since 2000",
     instaUrl: "#",
     twitUrl: "#",
     website: "#",
     imgUrl: black_decker,
+  },
+  {
+    name: "Hitachi",
+    since: "Since 2005",
+    instaUrl: "#",
+    twitUrl: "#",
+    website: "#",
+    imgUrl: hitachi,
+  },
+  {
+    name: "Bosch",
+    since: "Since 2003",
+    instaUrl: "#",
+    twitUrl: "#",
+    website: "#",
+    imgUrl: bosch,
+  },
+  {
+    name: "Dong Cheng",
+    since: "Since 2001",
+    instaUrl: "#",
+    twitUrl: "#",
+    website: "#",
+    imgUrl: dongcheng,
+  },
+  {
+    name: "Ferm",
+    since: "Since 2003",
+    instaUrl: "#",
+    twitUrl: "#",
+    website: "#",
+    imgUrl: ferm,
+  },
+  {
+    name: "Planet Power",
+    since: "Since 2000",
+    instaUrl: "#",
+    twitUrl: "#",
+    website: "#",
+    imgUrl: planet_power,
+  },
+  {
+    name: "Makita",
+    since: "Since 2005",
+    instaUrl: "#",
+    twitUrl: "#",
+    website: "#",
+    imgUrl: makita,
   },
 ];
 
